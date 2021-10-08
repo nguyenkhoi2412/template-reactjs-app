f3c727ee07a2e8e6150225990067d75669498121
Koala Khoi <nguyenkhoi2412@gmail.com>
2021-10-08 08:59:48 +0700

    .

--- src/app/routes/AppRouting.js
+++ src/app/routes/AppRouting.js
@@ -1,21 +1,29 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import routes from "./index";
 
 const AppRouting = ({ isAuthenticated = false }) => {
   return (
     <Router>
       <Suspense fallback={<>Loading...</>}>
         <Switch>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={Math.random()}
+                path={route.path}
+                component={route.component}
+              />
+            );
+          })}
           {/* add your routes & additional routings which for public user here. */}
-          <Route
+          {/* <Route
             path={"/"}
             component={lazy(() => import("../../containers/Home"))}
           />
           <Route
             path={"/about"}
             component={lazy(() => import("../../containers/About"))}
-          />
+          /> */}
           {/* add your routes & additional routings which need authenticated user here. */}
         </Switch>
       </Suspense>
@@ -24,3 +32,23 @@ const AppRouting = ({ isAuthenticated = false }) => {
 };
 
 export default AppRouting;
+
+const RELATIVE_PATH = process.env.RELATIVE_PATH || "/";
+
+const routes = [
+  //#region Home page
+  {
+    path: RELATIVE_PATH,
+    exact: true,
+    public: true,
+    title: "Welcome to my world!!!",
+    component: lazy(() => import("../../containers/Home")),
+  },
+  {
+    path: RELATIVE_PATH + "about",
+    exact: true,
+    public: true,
+    title: "Welcome to my world!!!",
+    component: lazy(() => import("../../containers/About")),
+  },
+];
