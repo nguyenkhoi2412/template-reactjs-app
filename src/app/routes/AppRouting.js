039a91123aebc94e11c7f3b276d07a1901bff9a9
Koala Khoi <nguyenkhoi2412@gmail.com>
2021-10-08 08:27:17 +0700

    .

--- src/app/routes/AppRouting.js
+++ src/app/routes/AppRouting.js
@@ -7,24 +7,15 @@ const AppRouting = ({ isAuthenticated = false }) => {
     <Router>
       <Suspense fallback={<>Loading...</>}>
         <Switch>
-          {routes.map((route, index) => {
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
           {/* add your routes & additional routings which for public user here. */}
-          {/* <Route
+          <Route
             path={"/"}
             component={lazy(() => import("../../containers/Home"))}
           />
           <Route
             path={"/about"}
             component={lazy(() => import("../../containers/About"))}
-          /> */}
+          />
           {/* add your routes & additional routings which need authenticated user here. */}
         </Switch>
       </Suspense>
