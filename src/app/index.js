759afe3e3e70b4aaf5c337084494e9e0de09d6fc
Koala Khoi <nguyenkhoi2412@gmail.com>
2021-10-08 07:56:31 +0700

    3

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@ import "./app.less";
 import React, { Suspense } from "react";
 import cmsFavicon from "@assets/favicons/dashboard/favicon.ico";
 import surveyFavicon from "@assets/favicons/survey/favicon.ico";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PagesRoute from "@utils/components/PagesRoute";
 import ProgressBar from "@components/common/ProgressBar";
 import gVariable from "@stores/shared/variables";
@@ -20,6 +20,9 @@ import BackdropSpin from "@components/common/BackdropSpin";
 // } from "@redux/providers/site.reducer";
 // import { TYPE_GET_BY_SITE } from "@redux/providers/type.reducer";
 import { Helpers } from "@utils/helpers";
+
+import Home from "../containers/Home";
+import About from "../containers/About";
 
 const App = () => {
   console.warn = () => {};
@@ -63,12 +66,17 @@ const App = () => {
         <Suspense fallback={<>Loading...</>}>
           <Switch>
             {routes.map((route, index) => (
-              <PagesRoute key={index} {...route}>
-                {route.component}
-              </PagesRoute>
+              // <PagesRoute key={index} {...route}>
+              //   {route.component}
+              // </PagesRoute>
+              <Route key={index} {...route} />
             ))}
           </Switch>
         </Suspense>
+        {/* <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch> */}
       </Router>
       {/** Snackbar show message results */}
       {/* <SnackbarmaUI /> */}
